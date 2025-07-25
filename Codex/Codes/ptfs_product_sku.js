3074f356a65aec39c65f5702797708c9eae69757
Dave8011 <saurabhdave8@gmail.com>
2025-07-25 16:37:33 +0530

    💾 Update Codex/Codes/ptfs_product_sku.js

--- Codex/Codes/ptfs_product_sku.js
+++ Codex/Codes/ptfs_product_sku.js
@@ -2,3 +2,5 @@ File with unique sku and with there variation count side by side.
 
 change to 45 from 35
 s clo to MediaKeyMessageEvent
+
+caps for non glass bottle and cap  to be listed on amazon, IndiaMART  to be listed.File
