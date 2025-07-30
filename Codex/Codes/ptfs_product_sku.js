5a90c8f6076188f1a9fe24c36b9b5f9f5f41b601
Dave8011 <saurabhdave8@gmail.com>
2025-07-30 17:40:32 +0530

    💾 Update Codex/Codes/ptfs_product_sku.js

--- Codex/Codes/ptfs_product_sku.js
+++ Codex/Codes/ptfs_product_sku.js
@@ -9,25 +9,25 @@ rack data for glass ware clearInterval
 
 
 New Listing:
-Mustard seeds big & small 1800 g bag--750--
-Ajwain Seeds 1800 g  bag--1 kg--
-Fennel Seeds 1800 g  bag--1 kg--
-Jeera Seeds 1800 g  bag--1 kg--
-Fenugreek Seeds 1800 g  bag--1 kg--
-Sona Masuri Brown & White Rice 10 KG & 5 KG
-Kolam Rice 10 kg & 5 kg
-Indriayani rice 10 kg & 5 kg
-basmati rice 10 kg & 5 kg
-idli rice 10 kg & 5 kg
-Ambemohar rice 10 kg 5 kg
-bay leaf 200 g
-red poha 1800 g
-Toor Dal 10 kg & 5 KG
-Moong DAl Yellow 10 kg & 5 kg
-Green Masoor Dal 10 kg 5 kg
-Chana Dal 10 kg & 5 kg
+Mustard seeds big & small 1800 g bag--750-- Done
+Ajwain Seeds 1800 g  bag--1 kg-- Done
+Fennel Seeds 1800 g  bag--1 kg-- Done
+Jeera Seeds 1800 g  bag--1 kg-- Done
+Fenugreek Seeds 1800 g  bag--1 kg-- Done
+Sona Masuri Brown & White Rice 10 KG & 5 KG Done
+Kolam Rice 10 kg & 5 kg Done
+Indriayani rice 10 kg & 5 kg Done
+basmati rice 10 kg & 5 kg Done
+idli rice 10 kg & 5 kg Done
+Ambemohar rice 10 kg 5 kg Done
+bay leaf 200 g Done
+red poha 1800 g Done
+Toor Dal 10 kg & 5 KG Done
+Moong DAl Yellow 10 kg & 5 kg Done
+Green Masoor Dal 10 kg 5 kg Done
+Chana Dal 10 kg & 5 kg Done
 urad dal 10 kg & 5 kg
 black urad dal 10 kg & 5 kg
-ragi 5 kg & 10 kg
-jowar 5 kg & 10 kg
-bajra 5 kg & 10 kg
+ragi 5 kg & 10 kg Done
+jowar 5 kg & 10 kg Done
+bajra 5 kg & 10 kg Done
