655c4eb0786592c0adeac7d9804c03c2de89d729
Dave8011 <saurabhdave8@gmail.com>
2025-07-30 16:29:59 +0530

    💾 Update Codex/Codes/ptfs_product_sku.js

--- Codex/Codes/ptfs_product_sku.js
+++ Codex/Codes/ptfs_product_sku.js
@@ -5,3 +5,29 @@ s clo to MediaKeyMessageEvent
 
 caps for non glass bottle and cap  to be listed on amazon, IndiaMART  to be listed.File
 rack data for glass ware clearInterval
+
+
+
+New Listing:
+Mustard seeds big & small 1800 g bag--750--
+Ajwain Seeds 1800 g  bag--1 kg--
+Fennel Seeds 1800 g  bag--1 kg--
+Jeera Seeds 1800 g  bag--1 kg--
+Fenugreek Seeds 1800 g  bag--1 kg--
+Sona Masuri Brown & White Rice 10 KG & 5 KG
+Kolam Rice 10 kg & 5 kg
+Indriayani rice 10 kg & 5 kg
+basmati rice 10 kg & 5 kg
+idli rice 10 kg & 5 kg
+Ambemohar rice 10 kg 5 kg
+bay leaf 200 g
+red poha 1800 g
+Toor Dal 10 kg & 5 KG
+Moong DAl Yellow 10 kg & 5 kg
+Green Masoor Dal 10 kg 5 kg
+Chana Dal 10 kg & 5 kg
+urad dal 10 kg & 5 kg
+black urad dal 10 kg & 5 kg
+ragi 5 kg & 10 kg
+jowar 5 kg & 10 kg
+bajra 5 kg & 10 kg
